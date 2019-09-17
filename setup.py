a2c56a857a3a52e46595b9de976d9ce82c25896b
Werner Van Geit <werner.vangeit@epfl.ch>
2019-09-17 15:42:17 +0200

    And another super fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     from urllib import urlretrieve
 
-VERSION = '0.2.5'
+VERSION = '0.2.6'
 
 NRN_SHORT_VERSION = '7.6'
 NRN_LONG_VERSION = '7.6.7'
@@ -34,7 +34,7 @@ class build_ext(build_ext_orig, object):
     def run(self):
         for ext in self.extensions:
             self.build_nrn(ext)
-        super(NrnExtension, self).run()
+        super(build_ext, self).run()
 
     def build_nrn(self, ext):
         os.makedirs(self.build_temp)
